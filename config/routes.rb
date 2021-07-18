5583516f51b5c0adb626174c25f116be42ac573d
edita21 <ekuberk0@chicagobooth.edu>
2021-07-18 03:26:16 +0000

    66% committ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get("/forex", { :controller => "currencies", :action => "first_currency"})
 
-
+get("/forex/:from_currency", { :controller => "currencies", :action => "second_currency"})
 
 end
